3277e9149243ac6f3e476b7e8d52f9e18e1ab9a1
anda <ando6160@gmail.com>
2022-09-06 23:32:11 +0900

    ⚡ :: [project-shooting] 20th

--- index.js
+++ index.js
@@ -97,6 +97,27 @@ const projectile = new Projectile(
   }
 );
 
+class Boss {
+  constructor(x, y, radius, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    cvs.beginPath();
+    cvs.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    cvs.fillStyle = this.color;
+    cvs.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 class Enemies {
   constructor(x, y, radius, velocity) {
     this.x = x;
@@ -220,32 +241,34 @@ function spawnSuperItem() {
 }
 
 function spawnEnemies() {
-  let num = 1;
-  setInterval(() => {
-    if (num <= 12) {
-      num += 0.05;
-    }
-    let radius = Math.random() * 30;
-    if (radius < 7) {
-      radius = 25;
-    }
-    let x;
-    let y;
-    if (Math.random() < 0.5) {
-      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
-      y = Math.random() * canvas.height;
-    } else {
-      x = Math.random() * canvas.width;
-      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
-    }
-    const angle = Math.atan2(canvas.width / 2 - x, canvas.height / 2 - y);
-    let velocity;
-    velocity = {
-      x: Math.sin(angle) * num,
-      y: Math.cos(angle) * num,
-    };
-    enemies.push(new Enemies(x, y, radius, velocity));
-  }, 1000);
+  if (scores >= 60) {
+    let num = 1;
+    setInterval(() => {
+      if (num <= 12) {
+        num += 0.05;
+      }
+      let radius = Math.random() * 30;
+      if (radius < 7) {
+        radius = 25;
+      }
+      let x;
+      let y;
+      if (Math.random() < 0.5) {
+        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+        y = Math.random() * canvas.height;
+      } else {
+        x = Math.random() * canvas.width;
+        y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+      }
+      const angle = Math.atan2(canvas.width / 2 - x, canvas.height / 2 - y);
+      let velocity;
+      velocity = {
+        x: Math.sin(angle) * num,
+        y: Math.cos(angle) * num,
+      };
+      enemies.push(new Enemies(x, y, radius, velocity));
+    }, 1000);
+  }
 }
 
 let cnt = 0;
@@ -281,7 +304,8 @@ function animate() {
         }
       });
     });
-    enemies.forEach((enemy, index) => {
+    enemies.forEach((enem
+      y, index) => {
       enemy.update();
       const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
       if (dist - enemy.radius - player.radius < 0) {
